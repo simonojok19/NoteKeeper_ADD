f7549d454808311be9ad84e852179bcb4da0cd94
simonojok19 <simonojok19@gmail.com>
2020-12-16 17:49:08 +0300

    finished module

--- app/src/main/java/co/simonojok19/notekeeper/DataManager.kt
+++ app/src/main/java/co/simonojok19/notekeeper/DataManager.kt
@@ -1,4 +1,24 @@
 package co.simonojok19.notekeeper
 
 class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>();
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo(courseId = "android_intents", title = "Android Programming with intents")
+        courses[course.courseId] = course
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses[course.courseId] = course
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses[course.courseId] = course
+
+        course = CourseInfo(courseId = "java_core", title = "Java Fundamentals: The Core Platform")
+        courses[course.courseId] = course
+    }
 }
