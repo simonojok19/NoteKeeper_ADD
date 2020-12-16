1007e38e89a8c58227bc26768aedb734abb749d5
simonojok19 <simonojok19@gmail.com>
2020-12-16 18:54:35 +0300

    now using data manager class names

--- app/src/main/java/co/simonojok19/notekeeper/MainActivity.kt
+++ app/src/main/java/co/simonojok19/notekeeper/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val dm = DataManager()
-        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item, dm.courses.values.toList())
+        val adapterCourses = ArrayAdapter<CourseInfo>(this, android.R.layout.simple_spinner_item, DataManager.courses.values.toList())
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinnerCourses.adapter = adapterCourses
