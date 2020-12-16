62ca020e18f579566f0768abd554c5033a93ac4c
simonojok19 <simonojok19@gmail.com>
2020-12-16 19:16:47 +0300

    removed un used codes

--- app/src/main/java/co/simonojok19/notekeeper/Constants.kt
+++ app/src/main/java/co/simonojok19/notekeeper/Constants.kt
@@ -1,3 +1,4 @@
 package co.simonojok19.notekeeper
 
 const val EXTRA_NOTE_POSITION = "EXTRA_NOTE_POSITION"
+const val POSITION_NOTE_SET = -1
--- app/src/main/java/co/simonojok19/notekeeper/MainActivity.kt
+++ app/src/main/java/co/simonojok19/notekeeper/MainActivity.kt
@@ -1,19 +1,17 @@
 package co.simonojok19.notekeeper
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private var notePosition = POSITION_NOTE_SET
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
