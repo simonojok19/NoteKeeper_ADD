aa22c3a0de3965fda041e65e4f10a24e3f86b591
simonojok19 <simonojok19@gmail.com>
2020-12-20 16:02:46 +0300

    test also working

--- app/src/test/java/co/simonojok19/notekeeper/DataManagerTest3.kt
+++ app/src/test/java/co/simonojok19/notekeeper/DataManagerTest3.kt
@@ -1,6 +1,7 @@
 package co.simonojok19.notekeeper
 
 import junit.framework.TestCase
+import org.junit.Assert
 
 class DataManagerTest3 : TestCase() {
 
@@ -14,5 +15,20 @@ class DataManagerTest3 : TestCase() {
 
     fun testGetNotes() {}
 
-    fun testAddNote() {}
+    fun testAddNote() {
+        // Prepare
+        val course = DataManager.courses["android_async"]!!
+        val noteTitle = "This is a test note"
+        val noteText = "This is the body of my test note"
+
+        // Act
+        val index = DataManager.addNote(course, noteTitle, noteText)
+
+        val note = DataManager.notes[index]
+
+        // Assert
+        Assert.assertEquals(course, note.course)
+        Assert.assertEquals(noteText, note.text)
+        Assert.assertEquals(noteTitle, note.title)
+    }
 }
