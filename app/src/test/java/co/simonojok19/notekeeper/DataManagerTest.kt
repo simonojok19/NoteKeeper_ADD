50940e3a44f77f023455ff2405cdc422610413ec
simonojok19 <simonojok19@gmail.com>
2020-12-20 15:56:39 +0300

    using local junit test

--- app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
+++ app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
@@ -1,16 +1,29 @@
 package co.simonojok19.notekeeper
 
-import junit.framework.TestCase
+import org.junit.Test
 
-class DataManagerTest : TestCase() {
+import org.junit.Assert.*
 
+
+class DataManagerTest {
+
+    @Test
     fun testAddNote() {
+        // Prepare
         val course = DataManager.courses["android_async"]!!
         val noteTitle = "This is a test note"
         val noteText = "This is the body of my test note"
 
+        // Act
         val index = DataManager.addNote(course, noteTitle, noteText)
 
         val note = DataManager.notes[index]
+
+        // Assert
+        assertEquals(course, note.course)
+        assertEquals(noteText, note.text)
+        assertEquals(noteTitle, note.title)
+
+
     }
 }
