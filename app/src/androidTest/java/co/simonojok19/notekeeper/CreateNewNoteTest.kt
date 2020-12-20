b79d6abd44d9fa882a8fbdcb8abd84856f086d46
simonojok19 <simonojok19@gmail.com>
2020-12-20 17:20:09 +0300

    started coding the test

--- app/src/androidTest/java/co/simonojok19/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/co/simonojok19/notekeeper/CreateNewNoteTest.kt
@@ -2,16 +2,18 @@ package co.simonojok19.notekeeper
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class CreateNewNoteTest {
 
     @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val noteListActivity = ActivityScenarioRule(NoteListActivity::class.java)
 
     @Test
     fun createNewNote() {
