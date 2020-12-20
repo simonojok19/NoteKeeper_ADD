4cbc928c0576724b72136cdfa1c00c4cf754f771
simonojok19 <simonojok19@gmail.com>
2020-12-20 16:17:48 +0300

    finished the index things

--- app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
+++ app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
@@ -24,4 +24,27 @@ class DataManagerTest {
         assertEquals(noteText, note.text)
         assertEquals(noteTitle, note.title)
     }
+
+    @Test
+    fun findSimilarNotes() {
+        // Prepare
+        val course = DataManager.courses["android_async"]!!
+        val noteTitle = "This is a test note"
+        val noteText1 = "This is the body of my test note"
+        val noteText2 = "This is the body of my second test note"
+
+        // Act
+        val index1 = DataManager.addNote(course, noteTitle, noteText1)
+        val index2 = DataManager.addNote(course, noteTitle, noteText2)
+
+        val note1 = DataManager.findNote(course, noteTitle, noteText1)
+        val foundIndex1 = DataManager.notes.indexOf(note1)
+
+        val note2 = DataManager.findNote(course, noteTitle, noteText2)
+        val foundIndex2 = DataManager.notes.indexOf(note2)
+
+        // Assert
+        assertEquals(index1, foundIndex1)
+        assertEquals(index2, foundIndex2)
+    }
 }
