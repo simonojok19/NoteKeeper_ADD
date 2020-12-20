5f5967282a0354270e0c29cb65a44ec4eda28940
simonojok19 <simonojok19@gmail.com>
2020-12-20 18:37:21 +0300

    added next through

--- app/src/androidTest/java/co/simonojok19/notekeeper/NextThroughNoteTest.kt
+++ app/src/androidTest/java/co/simonojok19/notekeeper/NextThroughNoteTest.kt
@@ -1,12 +1,16 @@
 package co.simonojok19.notekeeper
 
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class NextThroughNoteTest {
@@ -16,6 +20,25 @@ class NextThroughNoteTest {
 
     @Test
     fun nextThroughNotes() {
+        // Prepare
 
+
+        // Act
+        val listViewDataInteraction = onData(allOf(instanceOf(NoteInfo::class.java), equalTo(DataManager.notes[0])))
+        listViewDataInteraction.perform(click())
+        for (index in 0..DataManager.notes.lastIndex) {
+            val note = DataManager.notes[index]
+
+            onView(withId(R.id.spinnerCourses))
+                .check(matches(withSpinnerText(note.course?.title)))
+
+            onView(withId(R.id.textNoteTitle))
+                .check(matches(withText(note.title)))
+
+            onView(withId(R.id.textNoteText))
+                .check(matches(withText(note.text)))
+        }
+
+        // Assert
     }
 }
