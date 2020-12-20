86653b037d41d800c6f7c0ea9e259d44bbebbe20
simonojok19 <simonojok19@gmail.com>
2020-12-20 15:42:44 +0300

    adding test

--- app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
+++ app/src/test/java/co/simonojok19/notekeeper/DataManagerTest.kt
@@ -4,5 +4,13 @@ import junit.framework.TestCase
 
 class DataManagerTest : TestCase() {
 
-    fun testAddNote() {}
+    fun testAddNote() {
+        val course = DataManager.courses["android_async"]!!
+        val noteTitle = "This is a test note"
+        val noteText = "This is the body of my test note"
+
+        val index = DataManager.addNote(course, noteTitle, noteText)
+
+        val note = DataManager.notes[index]
+    }
 }
