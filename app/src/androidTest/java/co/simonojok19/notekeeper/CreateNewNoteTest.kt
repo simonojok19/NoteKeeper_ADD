ca5670e1e98e99d8f725b7c499420a21095a2578
simonojok19 <simonojok19@gmail.com>
2020-12-20 16:43:04 +0300

    added the runner

---
+++ app/src/androidTest/java/co/simonojok19/notekeeper/CreateNewNoteTest.kt
@@ -0,0 +1,15 @@
+package co.simonojok19.notekeeper
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CreateNewNoteTest {
+
+    @Test
+    fun createNewNote() {
+
+    }
+}
