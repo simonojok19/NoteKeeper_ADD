49afd73aabf0bf3b046af0f02f64a092b5495447
simonojok19 <simonojok19@gmail.com>
2020-12-20 18:47:23 +0300

    used espresso test recorder

---
+++ app/src/androidTest/java/co/simonojok19/notekeeper/NoteListActivityTest.kt
@@ -0,0 +1,89 @@
+package co.simonojok19.notekeeper
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.*
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class NoteListActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(NoteListActivity::class.java)
+
+    @Test
+    fun noteListActivityTest() {
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.fab),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        val appCompatSpinner = onView(
+            allOf(
+                withId(R.id.spinnerCourses),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                        1
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatSpinner.perform(click())
+
+        val appCompatCheckedTextView = onData(anything())
+            .inAdapterView(
+                childAtPosition(
+                    withClassName(`is`("android.widget.PopupWindow$PopupBackgroundView")),
+                    0
+                )
+            )
+            .atPosition(2)
+        appCompatCheckedTextView.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
