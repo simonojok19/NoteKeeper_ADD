e917ac2c6d114e8da8ad26f41ddf633b6dbc7751
simonojok19 <simonojok19@gmail.com>
2020-12-20 17:13:55 +0300

    using activity scenario rule

--- app/src/androidTest/java/co/simonojok19/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/co/simonojok19/notekeeper/CreateNewNoteTest.kt
@@ -1,12 +1,17 @@
 package co.simonojok19.notekeeper
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun createNewNote() {
