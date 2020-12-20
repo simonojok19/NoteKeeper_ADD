3334f382474515cd6fb9c0ec2a43480b29f55f79
simonojok19 <simonojok19@gmail.com>
2020-12-20 15:50:55 +0300

    working with testing

---
+++ app/src/test/java/co/simonojok19/notekeeper/DataManagerTest3.kt
@@ -0,0 +1,18 @@
+package co.simonojok19.notekeeper
+
+import junit.framework.TestCase
+
+class DataManagerTest3 : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testGetCourses() {}
+
+    fun testGetNotes() {}
+
+    fun testAddNote() {}
+}
