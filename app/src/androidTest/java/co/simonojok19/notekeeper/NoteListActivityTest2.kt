6ab78f47425e4e4a0af6dea7f1d5510c0e8264bc
simonojok19 <simonojok19@gmail.com>
2020-12-21 21:51:25 +0300

    added activity

---
+++ app/src/androidTest/java/co/simonojok19/notekeeper/NoteListActivityTest2.kt
@@ -0,0 +1,76 @@
+package co.simonojok19.notekeeper
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.*
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class NoteListActivityTest2 {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(NoteListActivity::class.java)
+
+    @Test
+    fun noteListActivityTest2() {
+        val materialTextView = onData(anything())
+                .inAdapterView(allOf(withId(R.id.list_notes),
+                        childAtPosition(
+                                withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
+                                0)))
+                .atPosition(0)
+        materialTextView.perform(click())
+
+        val overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        1),
+                                1),
+                        isDisplayed()))
+        overflowMenuButton.perform(click())
+
+        val materialTextView2 = onView(
+                allOf(withId(R.id.title), withText("Settings"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()))
+        materialTextView2.perform(click())
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
