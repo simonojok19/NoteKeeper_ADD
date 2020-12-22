b9865507533e11f75c118faee9fec915d5e7088d
simonojok19 <simonojok19@gmail.com>
2020-12-22 12:57:24 +0300

    all

--- app/src/main/java/com/jwhh/notekeeper/ColorSelector.kt
+++ app/src/main/java/com/jwhh/notekeeper/ColorSelector.kt
@@ -1,7 +1,26 @@
 package com.jwhh.notekeeper
 
 import android.content.Context
+import android.support.annotation.StyleableRes
+import android.util.AttributeSet
 import android.widget.LinearLayout
+import java.util.jar.Attributes
 
-class ColorSelector(context: Context): LinearLayout(context) {
+class ColorSelector: LinearLayout {
+
+    constructor(context: Context)
+            : super(context) {
+        orientation = HORIZONTAL
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet)
+            : super(context, attributeSet) {
+        orientation = HORIZONTAL
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int)
+            : super(context, attributeSet, defStyleAttr){
+        orientation = HORIZONTAL
+    }
+
 }
