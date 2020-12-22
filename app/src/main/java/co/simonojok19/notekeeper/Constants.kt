056dc666c98fe057918b383d91f40e6d542c263e
simonojok19 <simonojok19@gmail.com>
2020-12-22 12:43:51 +0300

    copied the project"

--- app/src/main/java/co/simonojok19/notekeeper/Constants.kt
+++
@@ -1,4 +0,0 @@
-package co.simonojok19.notekeeper
-
-const val NOTE_POSITION = "EXTRA_NOTE_POSITION"
-const val POSITION_NOTE_SET = -1
--- app/src/main/java/co/simonojok19/notekeeper/NoteKeeperData.kt
+++
@@ -1,9 +0,0 @@
-package co.simonojok19.notekeeper
-
-data class CourseInfo(val courseId: String, val title: String) {
-    override fun toString(): String {
-        return title
-    }
-}
-
-data class NoteInfo(var course: CourseInfo? = null, var title: String? = null, var text:String? = null)
