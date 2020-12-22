52cdda3ea085c6930e160e2fd95311d11a20da10
simonojok19 <simonojok19@gmail.com>
2020-12-22 08:29:27 +0300

    implemented the methods

--- app/src/main/java/co/simonojok19/notekeeper/NoteRecyclerAdapter.kt
+++ app/src/main/java/co/simonojok19/notekeeper/NoteRecyclerAdapter.kt
@@ -1,9 +1,22 @@
 package co.simonojok19.notekeeper
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteRecyclerAdapter: RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
