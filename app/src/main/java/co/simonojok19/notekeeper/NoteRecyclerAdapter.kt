a8f995ebf712d787cdd891eb70b0b9ef081ac15b
simonojok19 <simonojok19@gmail.com>
2020-12-22 08:41:34 +0300

    working with recycler onBindViewHolder

--- app/src/main/java/co/simonojok19/notekeeper/NoteRecyclerAdapter.kt
+++ app/src/main/java/co/simonojok19/notekeeper/NoteRecyclerAdapter.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+class NoteRecyclerAdapter(private val context: Context, private val notes: List<NoteInfo>): RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
     private val layoutInflater = LayoutInflater.from(context)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView  = layoutInflater.inflate(R.layout.item_note_list, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val note = notes[position]
+        holder.textCourse.text = note.course?.title ?: "No Title"
+        holder.textTitle.text = note.title
     }
 
     override fun getItemCount(): Int {
@@ -22,6 +26,7 @@ class NoteRecyclerAdapter(private val context: Context): RecyclerView.Adapter<No
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val textCourse = itemView.findViewById<TextView>(R.id.textCourse)
+        val textTitle = itemView.findViewById<TextView>(R.id.textTitle)
     }
 }
