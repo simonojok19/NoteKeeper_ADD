4674845177e6dc8a6479bf29ba56a5ea9b0a8fdc
simonojok19 <simonojok19@gmail.com>
2020-12-22 08:01:19 +0300

    working with recyclier view

--- app/src/androidTest/java/co/simonojok19/notekeeper/NoteListActivityTest2.kt
+++ app/src/androidTest/java/co/simonojok19/notekeeper/NoteListActivityTest2.kt
@@ -28,49 +28,5 @@ class NoteListActivityTest2 {
 
     @Test
     fun noteListActivityTest2() {
-        val materialTextView = onData(anything())
-                .inAdapterView(allOf(withId(R.id.list_notes),
-                        childAtPosition(
-                                withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                                0)))
-                .atPosition(0)
-        materialTextView.perform(click())
-
-        val overflowMenuButton = onView(
-                allOf(withContentDescription("More options"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.toolbar),
-                                        1),
-                                1),
-                        isDisplayed()))
-        overflowMenuButton.perform(click())
-
-        val materialTextView2 = onView(
-                allOf(withId(R.id.title), withText("Settings"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()))
-        materialTextView2.perform(click())
-    }
-
-    private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
