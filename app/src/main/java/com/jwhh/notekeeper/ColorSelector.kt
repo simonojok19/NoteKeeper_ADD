6842daf5b7f14f2a5f313521925f28cd109b2e54
simonojok19 <simonojok19@gmail.com>
2020-12-22 13:42:23 +0300

    added the layout

--- app/src/main/java/com/jwhh/notekeeper/ColorSelector.kt
+++ app/src/main/java/com/jwhh/notekeeper/ColorSelector.kt
@@ -1,14 +1,18 @@
 package com.jwhh.notekeeper
 
 import android.content.Context
+import android.graphics.Color
 import android.support.annotation.StyleableRes
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import java.util.jar.Attributes
 
-class ColorSelector constructor(context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0, defRes: Int = 0):
+class ColorSelector  @JvmOverloads
+constructor(context: Context, attributeSet: AttributeSet? = null, defStyle: Int = 0, defRes: Int = 0):
         LinearLayout(context, attributeSet, defStyle, defRes) {
 
+    private val listOfColors = listOf<Int>(Color.BLUE, Color.RED, Color.GREEN)
+    private var selectedColorIndex = 0
     init {
         orientation = HORIZONTAL
     }
